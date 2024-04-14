7cecd1b9640a2fddb4044457fd8cab556f1c56ab
Raid-dev <a_rauf2004@mail.ru>
2024-04-14 17:42:35 +0000

    Added executable script

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,6 +11,9 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
+
 	if (tree != NULL)
 	{
 		binary_tree_inorder(tree->left, func);
