b559ad83b1f51676613f2492e205638cda92ee50
Raid-dev <a_rauf2004@mail.ru>
2024-04-15 01:55:02 +0000

    Added executable script

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,14 +17,14 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree->left != NULL)
 	{
-		left_h = 1 + binary_tree_height(tree->left);
+		left_h = 1 + binary_tree_balance(tree->left);
 	}
 	else
 		left_h = 0;
 
 	if (tree->right != NULL)
 	{
-		right_h = 1 + binary_tree_height(tree->right);
+		right_h = 1 + binary_tree_balance(tree->right);
 	}
 	else
 		right_h = 0;
