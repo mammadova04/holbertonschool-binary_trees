6ad281f09fda547b268a42727636b7dc18f7f13e
Raid-dev <a_rauf2004@mail.ru>
2024-04-15 00:10:55 +0000

    Added executable script

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: The binary tree
+ *
+ * Return: The balance factor.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+	{
+		left_h = 1 + binary_tree_height(tree->left);
+	}
+	else
+		left_h = 0;
+
+	if (tree->right != NULL)
+	{
+		right_h = 1 + binary_tree_height(tree->right);
+	}
+	else
+		right_h = 0;
+
+	return (left_h - right_h);
+}
